a3397b525f31cdbbbab6b6a093b6b459d26ab0eb
Algimantas Stancelis <algimantas.stancelis@geniussports.com>
2021-02-06 22:32:19 +0200

    Move GuitarSpec search into GuitarSpec class

--- src/lib/guitarSpec.ts
+++ src/lib/guitarSpec.ts
@@ -3,4 +3,32 @@ import { Builder, Type, Wood } from "./types";
 export class GuitarSpec {
     constructor(readonly builder: Builder, readonly model: string, readonly type: Type, readonly backWood: Wood,
         readonly topWood: Wood, readonly numStrings: number = 6) { }
+
+    matches(search: GuitarSpec): boolean {
+        if (search.builder !== null && this.builder !== search.builder) {
+            return false;
+        }
+
+        if (search.model !== null && this.model.toLowerCase() !== search.model.toLowerCase()) {
+            return false;
+        }
+
+        if (search.type !== null && this.type !== search.type) {
+            return false;
+        }
+
+        if (search.backWood !== null && this.backWood !== search.backWood) {
+            return false;
+        }
+
+        if (search.topWood !== null && this.topWood !== search.topWood) {
+            return false;
+        }
+
+        if (this.numStrings !== search.numStrings) {
+            return false;
+        }
+
+        return true;
+    }
 }
--- src/lib/inventory.ts
+++ src/lib/inventory.ts
@@ -24,45 +24,6 @@ export class Inventory {
     }
 
     search(searchSpec: GuitarSpec): Guitar[] {
-        let matchingGuitars: Guitar[] = []
-
-        for (let guitar of this.#guitars) {
-            let guitarSpec = guitar.spec
-            // Ignore serial number since that's unique
-            // Ignore price since that's unique
-
-            let builder = searchSpec.builder
-            if (builder !== null && builder !== guitarSpec.builder) {
-                continue;
-            }
-
-            let model = searchSpec.model;
-            if (model !== null && model.toLowerCase() !== guitarSpec.model.toLowerCase()) {
-                continue;
-            }
-
-            let type = searchSpec.type;
-            if (type !== null && type !== guitarSpec.type) {
-                continue;
-            }
-
-            let backWood = searchSpec.backWood;
-            if (backWood !== null && backWood !== guitarSpec.backWood) {
-                continue;
-            }
-
-            let topWood = searchSpec.topWood;
-            if (topWood !== null && topWood !== guitarSpec.topWood) {
-                continue;
-            }
-
-            if (searchSpec.numStrings !== guitarSpec.numStrings) {
-                continue;
-            }
-
-            matchingGuitars.push(guitar);
-        }
-
-        return matchingGuitars;
+        return this.#guitars.filter((guitar) => guitar.spec.matches(searchSpec))
     }
 }
