c8dfeeeaa65ef970af2b1adcca837feacd2f2df4
Algimantas Stancelis <algimantas.stancelis@gmail.com>
2021-02-08 13:31:34 +0200

    Make Guitar price changeable

--- src/ricksGuitars/lib/guitar.ts
+++ src/ricksGuitars/lib/guitar.ts
@@ -1,5 +1,5 @@
 import { GuitarSpec } from "./guitarSpec";
 
 export class Guitar {
-    constructor(readonly serialNumber: string, readonly price: number, readonly spec: GuitarSpec) { }
+    constructor(readonly serialNumber: string, public price: number, readonly spec: GuitarSpec) { }
 }
